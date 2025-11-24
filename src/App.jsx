62a463c48cd7fcb8f87585acd544f1d351a27407
Prashantww <prashant1warghude@gmail.com>
2025-11-24 14:26:48 +0530

    fixed navbar component link

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/Navbar";
 
 import Home from "./pages/Home";
 import Airdrop from "./pages/Airdrop";
